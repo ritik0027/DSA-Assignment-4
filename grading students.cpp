0aed025c607c166aff6b5e860fc4344c15932933
Ritik Kumar <114002245+ritik0027@users.noreply.github.com>
2022-10-10 12:25:24 +0530

    Create grading students.cpp

---
+++ grading students.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int n , x;
+    cin >> n ;
+    for(int i=0;i<n;i++){
+        cin >> x ;
+        if(x>=38 && x%5>=3){
+            while(x%5!=0){
+                x++ ;
+            }
+        }
+          cout << x << endl;
+    }
+  
+    return 0;
+}
