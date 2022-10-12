584e88a2cb4c1aae023db8b1303e05b42bebfa91
Ritik Kumar <114002245+ritik0027@users.noreply.github.com>
2022-10-12 11:48:58 +0530

    Update number line jump.cpp

--- number line jump.cpp
+++ number line jump.cpp
@@ -4,33 +4,11 @@ int main(){
     int x1,x2,v1,v2;
     cin >> x1 >> v1;
     cin >> x2 >> v2;
-    if(x1<x2){
-        if(v1<=v2){
-            cout << "NO" ;
-        }
-        else {
-            while(x1<=x2){
-                if(x1==x2){
-                    cout << "YES" ;
-                }
-                x1+=v1;
-                x2+=v2;
-            }
-        }
+    if((v1<=v2) || (x2-x1)%(v2-v1)){
+        cout << "NO" ;
     }
     else {
-        if(v2<=v1){
-            cout << "NO" ;
-        }
-        else {
-            while(x2<=x1){
-                if(x1==x2){
-                    cout << "YES" ;
-                }
-                x1+=v1;
-                x2+=v2;
-            }
-        } 
+        cout << "YES" ;
     }
     return 0;
 }
